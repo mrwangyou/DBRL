ea732b54ff754e70a37058468781cc9a35b7dd9a
mrwangyou <mrwangyou@qq.com>
2022-10-08 10:56:50 +0800

    Update RL baselines

--- src/models/sac_jsbsim.py
+++ src/models/sac_jsbsim.py
@@ -13,6 +13,9 @@ def parse_args():
     parser = argparse.ArgumentParser(description='TBD')
     # parser.add_argument('--modelPath', default='/data/wnn_data/bestModel/', metavar='str', help='specifies the pre-trained model')
     parser.add_argument('--playSpeed', default=0, metavar='double', help='specifies to run in real world time')
+    parser.add_argument('--train', action='store_true', help='specifies the running mode of DBRL')
+    parser.add_argument('--timesteps', default=10000000, metavar='double', help='specifies the training timesteps. Only works when --train is specified')
+    parser.add_argument('--test', action='store_true', help='specifies the running mode of DBRL')
     args = parser.parse_args()
     return args
 
@@ -36,29 +39,31 @@ path = r'./log/'
 if not os.path.exists(path):
     os.mkdir(path)
 
-# try:
-#     model.set_parameters("./log/sac_df2")
-# except:
-#     pass
-model.learn(total_timesteps=10000000, log_interval=1)
-model.save("./log/sac_df2")
+if args.train:
+    # try:
+    #     model.set_parameters("./log/sac_jsbsim")
+    # except:
+    #     pass
+    model.learn(total_timesteps=10000000, log_interval=1)
+    model.save("./log/sac_jsbsim")
 
-# model = DDPG.load("./log/sac_df2")
+if args.test:
+    model = SAC.load("./log/sac_jsbsim")
 
-win = 0
-episode = 0
+    win = 0
+    episode = 0
 
-obs = env.reset()
-while True:
-    action, _states = model.predict(obs)
-    obs, rewards, dones, info = env.step(action)
-    env.render()
-    if dones == True:
-        f = open('./log/sac_record.txt', 'a')
-        if rewards == 50:
-            win += 1
-        episode += 1
-        f.write("{} / {}\n".format(win, episode))
-        print("Done! episode: {}\tacc: {}".format(episode, win / episode))
-        time.sleep(2)
-        env.reset()
+    obs = env.reset()
+    while True:
+        action, _states = model.predict(obs)
+        obs, rewards, dones, info = env.step(action)
+        env.render()
+        if dones == True:
+            f = open('./log/sac_jsbsim_record.txt', 'a')
+            if rewards == 50:
+                win += 1
+            episode += 1
+            f.write("{} / {}\n".format(win, episode))
+            print("Done! episode: {}\tacc: {}".format(episode, win / episode))
+            time.sleep(2)
+            env.reset()
